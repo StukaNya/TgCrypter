63ed6bc9f84ff9a41cefa970882380335b44ec1b
Denis Logutko <dlogutko@ptsecurity.com>
2021-03-19 20:59:12 +0300

    update pin code storage test

--- storage/pin/pin_test.go
+++ storage/pin/pin_test.go
@@ -1,10 +1,11 @@
 package store
 
 import (
+	"context"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/sirupsen/logrus"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Test mock-DB
@@ -15,20 +16,26 @@ func TestInsertDB(t *testing.T) {
 	}
 	defer db.Close()
 
-	// Init store and log instance
-	logger := logrus.New()
-	logger.SetLevel(logrus.WarnLevel)
+	// Init store and context
+	ctx := context.Background()
 
-	st := NewStore(logger, db)
-	info := AppInfo{1, "Stellaris"}
+	st := NewPinCodeStorage(db)
+	err = st.InitTable(ctx)
+	if err != nil {
+		t.Fatalf("unable to init sql mock table: %v", err)
+	}
+	info := PinMock{
+		id:   uuid.NewV4(),
+		data: []byte{'0', 'x', 'd', 'e', 'a', 'd'},
+	}
 
 	// Init expected SQL query to mock DB
-	mock.ExpectExec("INSERT INTO apps").
-		WithArgs(info.AppID, info.Name).
+	mock.ExpectExec("INSERT INTO pin_code").
+		WithArgs(info.id, info.data).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	// Now we execute our method
-	err = st.InsertAppInfo(&info)
+	err = st.StorePinHash(ctx, info.id, info.data)
 	if err != nil {
 		t.Errorf("Error was not expected while updating stats: %s", err)
 	}
