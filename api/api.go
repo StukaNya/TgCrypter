02bfc9c1bce361cb5617b14b53f3c1082164c785
Denis Logutko <dlogutko@ptsecurity.com>
2021-03-22 18:56:39 +0300

    gracefully shutdown api server

--- api/api.go
+++ api/api.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"context"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/StukaNya/TgCrypter/model/session"
 
@@ -24,15 +27,39 @@ func NewAPIServer(log *logrus.Logger, config *ServerConfig, users session.UserRe
 	}
 }
 
-func (s *APIServer) Start() error {
+func (s *APIServer) Serve(ctx context.Context) error {
 	r := gin.Default()
 
 	r.POST("/user", s.registerUser)
 	r.GET("/user/:id", s.fetchUser)
 
+	server := http.Server{
+		Addr:    s.config.BindAddr,
+		Handler: r,
+		BaseContext: func(listener net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			s.logger.Fatal("Error during server listening: ", err)
+		}
+	}()
 	s.logger.Info("Server is listening, URL: ", s.config.BindAddr)
 
-	return r.Run(s.config.BindAddr)
+	// Gracefully shutdown HTTP server...
+	<-ctx.Done()
+	s.logger.Info("Server stopped")
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctxShutDown); err != nil {
+		s.logger.Fatal("Unable to shutdown server: ", err)
+	}
+	s.logger.Info("Gracefully shutdown server")
+
+	return nil
 }
 
 func ServerError(err error) gin.H {
--- cmd/tg-crypter/main.go
+++ cmd/tg-crypter/main.go
@@ -5,6 +5,9 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/StukaNya/TgCrypter/api"
 	store "github.com/StukaNya/TgCrypter/storage/user"
@@ -25,6 +28,16 @@ func main() {
 	// Init context
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// Init OS sigquit channel
+	sigquit := make(chan os.Signal, 1)
+	signal.Notify(sigquit, os.Interrupt, syscall.SIGINT)
+
+	go func() {
+		oscall := <-sigquit
+		log.Printf("syscall:%+v", oscall)
+		cancel()
+	}()
 
 	// Load config from YAML file
 	config, err := NewConfig()
@@ -57,7 +70,7 @@ func main() {
 
 	// Server startup
 	server := api.NewAPIServer(logger, &config.ServerConfig, userSt)
-	err = server.Start()
+	err = server.Serve(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
